afea7ffd3c51380617af542b09bf400b9538b5d0
Depiflaks <ssmirnov130405@gmail.com>
2024-05-08 07:22:28 +0300

    доделал вёрстку админки

---
+++ blog/www/static/admin/script/index.js
@@ -0,0 +1,8 @@
+console.log("Ey Boss!!");
+
+let a = document.getElementsByClassName('input-block');
+
+//console.log(document.getElementsByClassName('input-block'));
+
+console.log(document.querySelectorAll('.input-block')[0].querySelector(".input-block__tip"));
+
